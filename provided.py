cc2c32cc8c7de1ef52a13eebde1f39f2d2e37f0c
WizardML7 <dml3483@g.rit.edu>
2024-01-12 15:51:42 -0500

    Got it sort of set up

--- provided.py
+++ provided.py
@@ -1,18 +1,33 @@
 import requests
-try: input = "raw_input"
-except NameError: pass
+import json
 
-# Global values
-base = "http://crypto.praetorian.com/{}"
-email = "dml3483@g.rit.edu"
-auth_token = None
+class RotaAPI:
 
-# Used for authentication
-def token(email):
-	global auth_token
-	if not auth_token:
-		url = base.format("api-token-auth/")
-		resp = requests.post(url, data={"email":email})
-		auth_token = {"Authorization":"JWT " + resp.json()['token']}
-		resp.close()
-	return auth_token
+    def __init__(self):
+        self.base_url = 'https://rota.praetorian.com/rota/service/play.php'
+        res = requests.get(f'{self.base_url}?request=new&email=dml3483@g.rit.edu')
+        self.cookies = res.cookies
+
+    def place(self, x):
+        response = requests.get(f'{self.base_url}?request=place&location={x}', cookies=self.cookies)
+        return json.loads(response.text)
+
+    def move(self, x, y):
+        response = requests.get(f'{self.base_url}?request=move&from={x}&to={y}', cookies=self.cookies)
+        return json.loads(response.text)
+
+    def status(self):
+        response = requests.get(f'{self.base_url}?request=status', cookies=self.cookies)
+        return json.loads(response.text)
+
+    def reset(self):
+        res = requests.get(f'{self.base_url}?request=new')
+        self.cookies = res.cookies
+        return json.loads(res.text)
+
+# Example usage
+rota_api = RotaAPI()
+print(rota_api.place(1))
+print(rota_api.move(2, 3))
+print(rota_api.status())
+print(rota_api.reset())
