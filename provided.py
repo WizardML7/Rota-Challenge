d16160c07dcae61ded31a68a5ac0bb025afffaf5
Domenic Lo Iacono <89399375+WizardML7@users.noreply.github.com>
2024-01-12 14:46:47 -0500

    Create provided.py

---
+++ provided.py
@@ -0,0 +1,18 @@
+import requests
+try: input = "raw_input"
+except NameError: pass
+
+# Global values
+base = "http://crypto.praetorian.com/{}"
+email = "dml3483@g.rit.edu"
+auth_token = None
+
+# Used for authentication
+def token(email):
+	global auth_token
+	if not auth_token:
+		url = base.format("api-token-auth/")
+		resp = requests.post(url, data={"email":email})
+		auth_token = {"Authorization":"JWT " + resp.json()['token']}
+		resp.close()
+	return auth_token
